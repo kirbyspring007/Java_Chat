c1938ead44dd356c06edb88bfc1f3be4d650e3e6
JEON YONGSUK <49046888+kirbyspring007@users.noreply.github.com>
2019-06-04 18:27:41 +0900

    2019/06/04

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -82,7 +89,46 @@ public class Main extends Application {
 	//UIを作り出しでプログラムを動かすメソッド
 	@Override
 	public void start(Stage primaryStage) {
-	
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
+		
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setFont(new Font(15));
+		root.setCenter(textArea);
+		
+		Button toggleButton = new Button("始まる");
+		toggleButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+		root.setBottom(toggleButton);
+		
+		String IP = "127.0.0.1";
+		int port = 9876;
+		
+		toggleButton.setOnAction(event -> {
+			if(toggleButton.getText().equals("始まる")) {
+				startServer(IP, port);
+				Platform.runLater(() -> {
+					String message = String.format("[サーバー接続]\n", IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("終了する");
+				});
+			} else {
+				stopServer();
+				Platform.runLater(() -> {
+					String message = String.format("[サーバース終了]\n", IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("始まる");
+				});
+			}
+		});
+		
+		
+		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setTitle("チャットサーバー");
+		primaryStage.setOnCloseRequest(event -> stopServer());
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	//プログラムの侵入
